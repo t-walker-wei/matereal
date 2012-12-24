3446f400810d1ce076134e9b2f70f54ef06027aa
arc.recoil@gmail.com <arc.recoil@gmail.com@865cba7d-088e-0b74-7e4a-8b7ddc60af70>
2012-12-24 00:01:30 +0000

    added a build script.

---
+++ realmote/trunk/realmote_build.cmd
@@ -0,0 +1,1 @@
+javac -classpath .\lib\connector-1.0.4.jar;.\lib\bluecove-2.1.1-SNAPSHOT.jar;.\lib\RXTXcomm.jar;.\lib\phybots-core-1.0.0.jar -d bin -sourcepath src -source 1.5 src\jp\digitalmuseum\rm\*.java src\jp\digitalmuseum\rm\handler\*.java
